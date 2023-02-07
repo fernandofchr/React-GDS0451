ce7a37240c8ce307b0e268dcb0f3936a63102e57
fernandofchr <114528947+fernandofchr@users.noreply.github.com>
2023-02-07 11:13:28 -0600

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello Angular!</h1>
+      <p>Empezamooos a react caray!! :)</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# React-GDS0451
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-ybea61)
